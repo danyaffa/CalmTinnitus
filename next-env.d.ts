cce790e3f644f61a035a7aa76443569dfc8767c2
Dan Leffler <leffleryd@gmail.com>
2025-11-18 10:31:45 +1100

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited.
+// Any edits will be overwritten on the next build.
