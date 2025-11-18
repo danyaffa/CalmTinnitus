20898b78634b1de4aa2ae0b08f925944954123e5
Dan Leffler <leffleryd@gmail.com>
2025-11-18 14:47:52 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // On the client bundle, ignore the Node-only "undici" package
-    if (!isServer) {
-      config.resolve = config.resolve || {};
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        undici: false,
-      };
-    }
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      undici: false
+    };
     return config;
-  },
+  }
 };
 
 export default nextConfig;
-
