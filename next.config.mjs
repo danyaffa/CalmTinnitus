e1d2bdd0b5a323c2135578c8760643b1389b12e6
Dan Leffler <leffleryd@gmail.com>
2025-12-11 13:19:40 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',               // <--- ADD THIS (Required for Capacitor)
+  images: {
+    unoptimized: true,            // <--- ADD THIS (Required if you use <Image> tags)
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve = config.resolve || {};
