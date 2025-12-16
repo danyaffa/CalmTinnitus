19c81b34717d129a8c9f5286abb91264717711c7
Dan Leffler <leffleryd@gmail.com>
2025-12-16 12:08:13 +1100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // <--- COMMENTED OUT FOR VERCEL DEPLOYMENT. (Uncomment this line only when building for Android/iOS)
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  assetPrefix: "./",
+  trailingSlash: true,
+  images: { unoptimized: true },
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true, // Ignores TS errors to force the build
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Ignores linting errors to force the build
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   webpack: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      undici: false
+      undici: false,
     };
     return config;
-  }
+  },
 };
 
 export default nextConfig;
+
